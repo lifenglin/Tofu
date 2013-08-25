2998913796bd990e9c012070219b121b69570d73
lifenglin <364613883@qq.com>
2013-08-25 22:21:10 +0800

    更新timer格式

--- src/Timer.php
+++ src/Timer.php
@@ -1,27 +1,72 @@
 <?php
+/**
+ * Tofu_Timer
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   Tofu
+ * @author    lifenglin <lifenglin1987@gmail.com>
+ * @copyright 2013 lifenglin
+ * @license   https://github.com/lifenglin/Tofu BSD Licence
+ * @version   SVN: <svn_id>
+ * @link      https://github.com/lifenglin/Tofu
+ */
+
+/**
+ * Tofu_Timer
+ *
+ * @category  PHP
+ * @package   Tofu
+ * @author    lifenglin <lifenglin1987@gmail.com>
+ * @copyright 2013 lifenglin1987@gmail.com
+ * @license   https://github.com/lifenglin/Tofu BSD Licence
+ * @version   Release: <package_version>
+ * @link      https://github.com/lifenglin/Tofu
+ */
 class Tofu_Timer extends Tofu_Core
 {
-    static protected $_arrData = array();
+    static protected $arrData = array();
 
+    /**
+     * record begin time
+     *
+     * @see process()
+     */
     static public function begin($strName = 'default')
     {
-        return self::$_arrData[$strName]['begin'] = self::$_arrData[$strName]['end'] = microtime(true);
+        return self::$arrData[$strName]['begin'] = self::$arrData[$strName]['end'] = microtime(true);
     }
 
+    /**
+     * record end time
+     *
+     * @see process()
+     */
     static public function end($strName = 'default')
     {
-        return self::$_arrData[$strName]['end'] = microtime(true);
+        return self::$arrData[$strName]['end'] = microtime(true);
     }
 
+    /**
+     * count time
+     *
+     * @see process()
+     */
     static public function count($strName = 'default')
     {
-        return round((self::$_arrData[$strName]['end'] - self::$_arrData[$strName]['begin']) * 1000);
+        return round((self::$arrData[$strName]['end'] - self::$arrData[$strName]['begin']) * 1000);
     }
 
+    /**
+     * count all time
+     *
+     * @see process()
+     */
     static public function countAll($strName = 'default')
     {
         $arrCount = array();
-        $arrKeys = array_keys(self::$_arrData);
+        $arrKeys = array_keys(self::$arrData);
         foreach ($arrKeys as $strName) {
             $arrCount[$strName] = self::count($strName);
         }
